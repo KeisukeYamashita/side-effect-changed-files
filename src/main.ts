e11884409705bcd9433029364cb34f4d1b6e7436
KeisukeYamashita <19yamashita15@gmail.com>
2024-07-27 18:59:31 +0200

    feat: use multiline inputs
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import YAML from "yaml";
 import type { Config } from "./config";
 import { map } from "./map";
+import * as util from "./util";
 
 /**
  * The main function for the action.
@@ -11,11 +12,11 @@ import { map } from "./map";
 export async function run(): Promise<void> {
 	try {
 		const config: Config = {
-			bypass: core.getMultilineInput("bypass", { required: false }),
+			bypass: util.getMultilineInput("bypass", { required: false }),
 			dirNames: core.getInput("dir_names", { required: true }) === "true",
 			escape_json: core.getInput("escape_json", { required: true }) === "true",
-			files: core.getInput("files", { required: false }).split(" "),
-			filters: core.getMultilineInput("filters", { required: false }),
+			files: util.getMultilineInput("files", { required: false }),
+			filters: util.getMultilineInput("filters", { required: false }),
 			include: core.getInput("include", { required: false }) === "true",
 			json:
 				core.getInput("json", { required: true }) === "true" ||
